c08f3166658ffba6fc59bf7ff77db44c831a87e5
Corporate Admin <iswcorp@ISW-220521-608.Interswitchng.com>
2023-08-24 15:08:20 +0100

    config commit

--- hospital-service/src/main/java/com/elegant/hospitalservice/repository/HospitalRepository.java
+++ hospital-service/src/main/java/com/elegant/hospitalservice/repository/HospitalRepository.java
@@ -2,5 +2,7 @@ package com.elegant.hospitalservice.repository;
 
 import com.elegant.hospitalservice.model.Hospital;
 
-public interface HospitalRepository extends JpaRepository<Hospital, Long>{
+
+
+public interface HospitalRepository{
 }
